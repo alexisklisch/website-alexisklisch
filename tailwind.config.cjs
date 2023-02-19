ce2017b34f59df56477d64ba7dff957f46e833ad
Alexis Fleitas Klisch <dev@alexisklisch.com>
2023-02-19 19:20:19 -0300

    Add footer and little components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "src/**/*.astro"],
   theme: {
     extend: {
+			animation: {
+				jello: 'jello 2s infinite'
+			},
+			keyframes: {
+				jello: {
+					'11.1%': { transform: 'translate3d(0, 0, 0)', transformOrigin: 'left' },
+					'22.2%': {transform: 'skewX(-12.5deg) skewY(-12.5deg)'},
+					'33.3%': {transform: 'skewX(6.25deg) skewY(6.25deg)'},
+					'44.4%': {transform: 'skewX(-3.125deg) skewY(-3.125deg)'},
+					'55.5%': {transform: 'skewX(1.5625deg) skewY(1.5625deg)'},
+					'66.6%': {transform: 'skewX(-0.78125deg) skewY(-0.78125deg)'},
+					'77.7%': {transform: 'skewX(0.390625deg) skewY(0.390625deg)'},
+					'88.8%': {transform: 'skewX(-0.1953125deg) skewY(-0.1953125deg)'}
+				}
+			},
       colors: {
         sixDark: {
 					100: "#BABBBF",
